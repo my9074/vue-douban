eab888cb5d037a1dd42101405a34e8c122302a11
my9074 <313231068@qq.com>
2017-03-01 22:28:54 +0800

    1.Update .gitignore file 2.Add NavMenu/Form/Table ui

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from 'components/Hello'
+import Form from 'views/Form'
+import Home from 'views/Home'
+import Table from 'views/Table'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      component: Home,
+      children: [
+        {
+          path: 'form',
+          component: Form
+        },
+        {
+          path: 'table',
+          component: Table
+        }
+      ]
+    },
+    {
+      path: '*',
+      redirect: '/form'
     }
   ]
 })
