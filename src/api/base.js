c9d40df06941afb3a539161079be77abc1f5457d
my9074 <313231068@qq.com>
2017-03-26 22:12:45 +0800

    Add element notification for ajax base.js

--- src/api/base.js
+++ src/api/base.js
@@ -2,14 +2,18 @@
  * Created by my9074 on 2017/3/21.
  */
 import axios from 'axios'
+import {Message} from 'element-ui'
 
 const HOST = '/api/'
 
 export default function (url, params = {}) {
   return new Promise((resolve, reject) => {
-    axios.get(HOST + url, { params })
+    axios.get(HOST + url, {params})
       .then((res) => {
         resolve(res.data)
-      }).catch(err => reject(err))
+      }).catch(err => {
+        Message.error('ajax error')
+        reject(err)
+      })
   })
 }
